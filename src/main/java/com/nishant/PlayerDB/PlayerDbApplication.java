d0cb03e11b1d9e9c3706e02c6475f1073976a98a
Nishant Rastogi <nishant015@gmail.com>
2023-10-23 06:56:58 +0530

    First Commit

---
+++ src/main/java/com/nishant/PlayerDB/PlayerDbApplication.java
@@ -0,0 +1,13 @@
+package com.nishant.PlayerDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerDbApplication.class, args);
+	}
+
+}
